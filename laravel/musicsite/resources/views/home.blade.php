017c85e50847f27e8e621d9de47b0163e139856f
Lakostt <lakostt1337@gmail.com>
2020-10-10 20:41:34 +0300

    Работает html, но css не пофиксил

--- laravel/musicsite/resources/views/home.blade.php
+++ laravel/musicsite/resources/views/home.blade.php
@@ -1,13 +1,123 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <link href="{{ asset('../css/style.css/') }}" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <title>Главная страница</title>
 </head>
 <body>
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__logo">Тут логотип и название</div>
+
+            <nav class="nav">
+                <a class="nav__link" href="#">Домой</a>
+                <a class="nav__link" href="#">Рейтинг</a>
+                <a class="nav__link" href="#">Контакты</a>
+                <a class="nav__link" href="#">О нас</a>
+                <a class="nav__link" href="#">FAQ</a>
+                <a class="nav__link active" href="#">Войти</a>
+            </nav>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="intro">
+        <div class="container">
+            <div class="intro__inner">
+                <h1 class="intro__title">Лучший сервис для фанатов музыки</h1>
+                <h2 class="intro__subtitle">Ну тут какой-то еще текст нужно типо да туда сюда летим</h2>
+
+                <a class="button__new" href="#">Войти в приложение</a>
+
+            </div>
+        </div>
+        <div class="slider">
+            <div class="container">
+                <div class="slider__inner">
+                    <div class="slider__item active">
+                        <span class="slider__num"></span>
+                        KAK DELA
+                    </div>
+                    <div class="slider__item">
+                        <span class="slider__num"></span>
+                        KAK DELA
+                    </div>
+                    <div class="slider__item">
+                        <span class="slider__num"></span>
+                        ETO NOVIY
+                    </div>
+                    <div class="slider__item">
+                        <span class="slider__num"></span>
+                        CADILAC
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="section">
+        <div class="container">
+            <div class="section__header">
+                <h3 class="section__suptitle">Поддерживаемые сервисы</h3>
+                <h2 class ="section__title">Что мы можем?</h2>
+                <div class="section__text">
+                    <p>Тут раньше был большой текст, но на этот язык выдавалась ошибка и пришлось поменять его на вот эту вот фигню.</p></div>
+            </div>
+        </div>
+
+        <div class="about">
+            <div class="about__item">
+                <div class="about__img">
+                    <img src="../Images/spotify.png" alt="">
+                </div>
+            </div>
+            <div class="about__item">
+                <div class="about__img">
+                    <img src="../Images/vk.png" alt="">
+                </div>
+            </div>
+            <div class="about__item">
+                <div class="about__img">
+                    <img src="../Images/yandex.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<!-- div class="statistics">
+    <div class="container">
+         <div class="stat">
+           <div class="stat__item">
+               <div class="stat__count">42</div>
+               <div class="stat__text">Онлайн</div>
+           </div>
+           <div class="stat__item">
+               <div class="stat__count">42</div>
+               <div class="stat__text">Пользователей</div>
+           </div>
+           <div class="stat__item">
+               <div class="stat__count">42</div>
+               <div class="stat__text">Воспроизведено треков</div>
+           </div>
+           <div class="stat__item">
+               <div class="stat__count">Number</div>
+               <div class="stat__text">Text</div>
+           </div>
+           <div class="stat__item">
+               <div class="stat__count">Number</div>
+               <div class="stat__text">Text</div>
+           </div>
+        </div>
+    </div>
+</div> -->
+<footer>
+    hello
+</footer>
 
 </body>
 </html>
+
